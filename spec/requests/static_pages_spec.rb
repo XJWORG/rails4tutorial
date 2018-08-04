2d9499384e36244c0b679643637be04cf391c1e5
xujunwei <xujunwei@cmos.chinamobile.com>
2018-08-04 10:00:24 +0800

    i

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,10 @@ describe "StaticPages" do
         visit '/static_pages/home'
         expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Sample App'" do
+        visit '/static_pages/home'
+        expect(page).to have_title('StaticPages#Sample App')
+    end
   end
 
   describe "Help Page" do
@@ -15,12 +19,20 @@ describe "StaticPages" do
         visit '/static_pages/help'
         expect(page).to have_content('Help')
     end
+    it "should have the title 'Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_title('StaticPages#Help')
+    end
   end
 
   describe "About" do
     it "should have the content 'About'" do
         visit '/static_pages/about'
         expect(page).to have_content('About')
+    end
+    it "should have the title 'About'" do
+        visit '/static_pages/about'
+        expect(page).to have_title('StaticPages#About')
     end
   end
 
