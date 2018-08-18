daa2986df5f546eb596d603905fb29abaea840f0
xujunwei <xujunwei@cmos.chinamobile.com>
2018-08-18 16:08:17 +0800

    fix: 9.2.2用户只能编辑自己的资料

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user , only: [:edit, :update]
+  before_action :correct_user ,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -25,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
@@ -44,4 +45,9 @@ class UsersController < ApplicationController
     redirect_to signin_url, notice: "Please sign in." unless sign_in?
   end
 
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
 end
