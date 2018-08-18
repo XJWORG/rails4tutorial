cc5a4c57602153b49315c67ce21295e8ded2a5f5
xujunwei <xujunwei@cmos.chinamobile.com>
2018-08-18 21:16:15 +0800

    fix: 用户列表分页

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
     factory :user do
-        name  "michael"
-        email "michael@example.com"
+        # name  "michael"
+        # email "michael@example.com"
+
+        sequence(:name) {|n| "Person_#{n}" }
+        sequence(:email) { |n| "persion_#{n}@example.com" } 
+
         password "fullcase"
         password_confirmation "fullcase"
 
