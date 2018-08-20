39bc386827a968be05a4d05f1b223dbefbb2ab87
xujunwei <xujunwei@cmos.chinamobile.com>
2018-08-20 21:26:16 +0800

    fix: 修改微博micropost路由跳转

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
   resources :sessions , only: [:new , :create , :destroy]
+  resources :microposts, only: [:create, :destroy]
   get 'static_pages/home'
 #  get 'static_pages/help'
   get 'static_pages/about'
