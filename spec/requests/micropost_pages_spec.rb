858e8e9bc06af935861b469b4fac2ca628c54d97
xujunwei <xujunwei@cmos.chinamobile.com>
2018-08-22 18:51:47 +0800

    fix: 修改调试信息（微博创建OK）

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe "MicropostPages", type: :request do
-  describe "GET /micropost_pages" do
-    it "works! (now write some real specs)" do
-      get micropost_pages_path
-      expect(response).to have_http_status(200)
-    end
-  end
 
   subject { page }
 
