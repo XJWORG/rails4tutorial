6a09b345a0463811d1271d2c35624eab170dca37
xujunwei <xujunwei@cmos.chinamobile.com>
2018-08-27 22:07:49 +0800

    fix: 子查询使用更高效的SQL方法

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,10 @@ class Micropost < ApplicationRecord
     default_scope  { order("created_at desc")}
 
     def self.from_users_followed_by(user)
-        followed_user_ids = user.followed_user_ids
-        where("user_id IN (?) OR user_id= ?", followed_user_ids, user)
+        followed_user_ids = "Select followed_id from relationships where follower_id = :user_id"
+        # where("user_id IN (?) OR user_id= ?", followed_user_ids, user)
+        # where("user_id In (:followed_user_ids) OR user_id = :user_id ",followed_user_ids: followed_user_ids, user_id: user)
+        # 更高效的内置方法
+        where("user_id in (#{followed_user_ids}) Or user_id = :user_id ", user_id: user.id)
     end
 end
